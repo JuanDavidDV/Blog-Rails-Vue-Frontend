cbba88fc4447c634fc40c0b62a125f94b9ea50b8
JuanDavidDV <juandadazav@gmail.com>
2025-05-27 11:04:47 -0400

    update website title and remove console log

--- src/composables/usePost.js
+++ src/composables/usePost.js
@@ -1,7 +1,6 @@
 import { ref, onMounted } from "vue";
 
 const API_URL = import.meta.env.VITE_API_URL + "/posts";
-console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const posts = ref([]);
 const title = ref("");
